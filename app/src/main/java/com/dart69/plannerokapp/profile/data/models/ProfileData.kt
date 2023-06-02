a3ca01c8b92d1106fc5ba28890312d52c76aea17
Alexander Myasnikov <al.next235@gmail.com>
2023-06-02 18:20:42 +0300

    Create profile layouts

---
+++ app/src/main/java/com/dart69/plannerokapp/profile/data/models/ProfileData.kt
@@ -0,0 +1,19 @@
+package com.dart69.plannerokapp.profile.data.models
+
+data class ProfileData(
+    val avatar: String?,
+    val avatars: String?,
+    val birthday: String?,
+    val city: String?,
+    val completed_task: Int,
+    val created: String,
+    val id: Int,
+    val instagram: String?,
+    val last: String?,
+    val name: String,
+    val online: Boolean,
+    val phone: String,
+    val status: String?,
+    val username: String,
+    val vk: String?
+)
---
+++ app/src/main/java/com/dart69/plannerokapp/profile/data/models/UserResponse.kt
@@ -0,0 +1,5 @@
+package com.dart69.plannerokapp.profile.data.models
+
+data class UserResponse(
+    val profile_data: ProfileData
+)
